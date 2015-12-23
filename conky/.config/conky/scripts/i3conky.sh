4597310cec74ca6307213f3cf937ddc220a73f67
astrohckr <astrohckr@gmail.com>
2015-12-23 14:34:36 -0600

    LE SIGH this is from the example, which works...

--- conky/.config/conky/scripts/i3conky.sh
+++ conky/.config/conky/scripts/i3conky.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
-echo "{ \"version\": 1, \"stop_signal\": 10, \"cont_signal\": 12, \"click_events\": true }"
-echo "[[],"
-conky -d -c ~/.config/conky/conky.conf
+
+# Send the header so that i3bar knows we want to use JSON:
+echo '{"version":1}'
+
+# Begin the endless array.
+echo '['
+
+# We send an empty first array of blocks to make the loop simpler:
+echo '[],'
+
+# Now send blocks with information forever:
+exec conky -c $HOME/.config/conky/conky.conf
